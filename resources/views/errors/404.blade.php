51ed8906761c1c25cd4c0b082a9851646ab6d4f8
Goular <zhaojt_cs@126.com>
2017-09-12 11:41:12 +0800

    提交内容

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -16,7 +16,6 @@
             BORDER-RIGHT: #e1e1e1 1px solid; BORDER-TOP: #e1e1e1 1px solid; MARGIN-TOP: 10px; BACKGROUND: #fff; BORDER-LEFT: #e1e1e1 1px solid; BORDER-BOTTOM: #e1e1e1 1px solid; HEIGHT: 585px; TEXT-ALIGN: center; border-radius: 10px
         }
     </style>
-
     <meta content="MSHTML 6.00.6000.17117" name="GENERATOR"></head>
 <body><section class="mod-page-body">
     <div class="mod-page-main wordwrap clearfix">
@@ -27,8 +26,5 @@
                 <p style="FONT-SIZE: 14px; LINE-HEIGHT: 20px">您可以回到 <a href="#">网站首页</a> 或到<a href="#" target="_blank">帮助中心</a> </p></div></div></div></section><footer class="mod-footer mod-cs-footer">
     <div class="clearfix hidden-box"></div>
     <div class="footer-box">
-
         <div class="copy-box">©2017&nbsp;加工屋</div></div></footer>
-
-
 </body></html>
